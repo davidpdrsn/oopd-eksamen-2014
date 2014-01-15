ae1e01d06c45f32cb993c34a32f0aaa66055cc0e
David Pedersen <david.pdrsn@gmail.com>
2014-01-15 20:20:25 +0100

    add empty owl tester

---
+++ test/models/OwlTester.java
@@ -0,0 +1,22 @@
+package test.models;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import app.models.*;
+
+@RunWith(JUnit4.class)
+public class OwlTester {
+  @Test
+  public void there_has_to_be_a_test() {}
+}
